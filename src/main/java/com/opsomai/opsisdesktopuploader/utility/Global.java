f102fcbc3ee9d3236d97ee472d754ae818ae4d5d
benzinho75 <benoit.clemenceau.zer@gmail.com>
2020-07-22 19:05:19 +0200

    Changed window dimensions

--- src/main/java/com/opsomai/opsisdesktopuploader/utility/Global.java
+++ src/main/java/com/opsomai/opsisdesktopuploader/utility/Global.java
@@ -6,17 +6,17 @@ package com.opsomai.opsisdesktopuploader.utility;
 public class Global {
 
     // If building without packaging :
-//    private static String workingDirPrefix = "src/main/";
-//    
-    // If building through macOS packaging :
-    private static String workingDirPrefix = "";
-//
-    // If building through Windows packaging :
-//    private static String workingDirPrefix = "?";
-//
-    // If building through Linux / Unix packaging :
-//    private static String workingDirPrefix = "?";
+    private static String workingDirPrefix = "src/main/";
 
+//    // If building through macOS packaging :
+//    private static String workingDirPrefix = "";
+//
+//    // If building through Windows packaging :
+//    private static String workingDirPrefix = "?";
+//
+//    // If building through Linux / Unix packaging :
+//    private static String workingDirPrefix = "?";
+//
     public static String getWorkingDirPrefix() {
         return Global.workingDirPrefix;
     }
